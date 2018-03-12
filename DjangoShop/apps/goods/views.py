b81e14d678fd2b8e863b24ad239216288a36d2e3
耿文浩 <gengwenhao97@126.com>
2018-03-12 14:12:14 +0800

    genericsListAPIView

--- DjangoShop/apps/goods/views.py
+++ DjangoShop/apps/goods/views.py
@@ -1,25 +1,23 @@
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 from .serializers import GoodsSerializer
 from .models import Goods
 
 
-class GoodsListView(APIView):
+class GoodsPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    page_query_param = 'p'
+    max_page_size = 100
+
+
+class GoodsListView(generics.ListAPIView):
     """
-        List all goods
+        商品列表页
     """
 
-    def get(self, request, format=None):
-        """
-            get
-        :param request:
-        :param format:
-        :return:
-        """
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
-
-        return Response(goods_serializer.data)
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
+    pagination_class = GoodsPagination
--- DjangoShop/settings.py
+++ DjangoShop/settings.py
@@ -33,6 +33,11 @@ ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'users.UserProfile'
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+#     'PAGE_SIZE': 10
+# }
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
